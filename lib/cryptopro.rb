abd4dfbf5eb7e49550cb79e1a189bfdc4a0e4770
Dmitry Koprov <dmitry.koprov@gmail.com>
2012-02-21 13:06:14 +0400

    Adds outer cofiguration for api_key for cabinet.ekey.ru

--- lib/cryptopro.rb
+++ lib/cryptopro.rb
@@ -4,4 +4,11 @@ require 'cryptopro/signature'
 require 'cryptopro/certificate'
 
 module Cryptopro
+
+  module Config
+
+    # API_KEY for cabinet.ekey.ru.
+    # Used for generating certificates, based in CSR, given by user.
+    mattr_accessor :api_key
+  end
 end
