92dfce9eefd7f4e98b991a43b657ecdb9d345c7a
Dmitry Koprov <dmitry.koprov@gmail.com>
2012-03-12 14:41:00 +0400

    API edited to harmonize with cabinet.ekey.ru,module usage description added.

--- lib/cryptopro/csr.rb
+++ lib/cryptopro/csr.rb
@@ -1,7 +1,27 @@
+# Модуль Csr отвечает за отправление запроса на сертификат (CSR) и получение готового сертификата.
+#
+# Существует 2 варианта работы с модулем:
+# 1. Тестовый выпуск - issue_test_certificate. Совмещает в себе отправку запроса
+# и получение тестового сертификата.
+# 2. Полноценный комплекс взаимодействия с ekey.ru:
+#    - issue -- отправляет запрос на выпуск сертификата
+#       на вход: csr в формате pem
+#       ответ: ruby hash в формате {"created_request_id" => <number>} или {"error" => <message>}
+#    - get_certificates -- забирает с Удостоверяющего центра (УЦ, CA) готовые сертификаты
+#       на вход: id сертификатов в очереди на выпуск (request_ids)
+#       ответ: json в формате [{"id" => <request_id>, "certificate" => <certificate_body>},
+#              если серт. еще не выпущен - {"id" => <request_id>, "error" => "certificate is not ready yet"}]
+
+require 'rest_client'
+require 'json'
+
 module Cryptopro
   class Csr
-    TEST_CA_UPLOAD_PAGE_URL = "http://www.cryptopro.ru/certsrv/certfnsh.asp"
+    TEST_CA_UPLOAD_PAGE_URL   = "http://www.cryptopro.ru/certsrv/certfnsh.asp"
     TEST_CA_DOWNLOAD_PAGE_URL = "http://www.cryptopro.ru/certsrv/certnew.cer?ReqID=%{ca_request_id}&Enc=b64"
+
+    CA_UPLOAD_PAGE_URL   = "http://cabinet.ekey.ru/api/1.0/request/put"
+    CA_DOWNLOAD_PAGE_URL = "http://cabinet.ekey.ru/api/1.0/certificates/get"
 
     def self.issue_test_certificate(csr)
       upload_uri = URI(TEST_CA_UPLOAD_PAGE_URL)
@@ -20,5 +40,21 @@ module Cryptopro
         raise "CSR not accepted"
       end
     end
+
+    def self.issue(csr)
+      upload_responce = RestClient.post(CA_UPLOAD_PAGE_URL,
+                                        { :api_key => Cryptopro::Config.api_key,
+                                          :pkcs10 => Cryptopro::Base.add_container_to_csr(csr) },
+                                        :multipart => true)
+      JSON(upload_responce)
+    end
+
+    def self.get_certificates(ca_request_ids)
+      id_list = Array(ca_request_ids).join(', ')
+      ca_responce = RestClient.post(CA_DOWNLOAD_PAGE_URL, { :api_key => Cryptopro::Config.api_key,
+                                                            :id_list => id_list })
+      JSON(ca_responce)
+    end
+
   end
 end
